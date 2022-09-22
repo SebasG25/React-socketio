f0779ddcb22b3529ffe4c09c58bcfe777e95588c
Sebastian Guzman <bitas200225@gmail.com>
2022-09-22 07:40:50 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new SocketServer(server, {
     cors: {
-        origin: 'http://localhost:3000' || 'https://react-socketio-vive-fest.vercel.app/',
+        // origin: 'http://localhost:3000' || 'https://react-socketio-vive-fest.vercel.app/',
     }
 })
 
